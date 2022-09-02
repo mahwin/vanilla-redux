c37a892a78a958187b6fd1636a629418ad75bff4
mahwin <mahwin7085@gmail.com>
2022-09-03 02:37:01 +0900

    Refactoring reducer -> createReducer toolkit

--- src/store.js
+++ src/store.js
@@ -1,20 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
 
-const reducers = (state = [], action) => {
-  switch (action.type) {
-    case addTodo.type:
-      console.log(action);
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteTodo.type:
-      return state.filter((todo) => todo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// createReducer를 사용하면  state를 mutate가능.
+// redux toolkit이 immer 아래에서 돌아가기 때문.
+// mutate하듯이 코드를 짜도 알아서 새로운 state을 리턴해줌.
+
+const reducers = createReducer([], (builder) => {
+  builder
+    .addCase(addTodo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteTodo, (state, action) =>
+      state.filter((todo) => todo.id !== action.payload)
+    );
+});
 
 export const actionCreators = {
   addTodo,
