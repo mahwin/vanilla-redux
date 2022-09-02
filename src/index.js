b52b6b42439452e55a1e361637e778ef54e11cc7
mahwin <mahwin7085@gmail.com>
2022-09-02 23:50:54 +0900

    Create  only use vanilla html and  redux

--- src/index.js
+++ src/index.js
@@ -1,42 +1,76 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const span = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-//Data modifier Fn
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const Add = "Add";
-const Minus = "Minus";
+const addToDo = (text) => {
+  return { type: ADD_TODO, text };
+};
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
 
-const modifier = (state = 0, action) => {
+// state is read-only!
+// Never Mutate State, only change through action!
+// state를 직접 변경하지 말고 새로운 state를 리턴해라!
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case Add:
-      return ++state;
-    case Minus:
-      return --state;
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
     default:
       return state;
   }
 };
-//{reducer: modifier Fn, middleware: [...middlewares]}
-const countStore = configureStore({ reducer: modifier });
 
-//action => modifier에게 특정 행동을 지시
-//action은 dispatch로 지시
-// countStore.dispatch({ type: "Minus" });
+const toDoStore = configureStore({ reducer: reducer });
 
-// getState()로 상태 값 얻기 가능!
-// console.log(countStore.getState());
-
-// 아래처럼 모디파이어 함수 안에다가 직접 변경 넣기 보다는
-// span.innerText = state;
-
-const onChange = () => {
-  span.innerText = countStore.getState();
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-countStore.subscribe(onChange);
+const paintToDos = () => {
+  const toDos = toDoStore.getState();
+  ul.innerHTML = "";
+  console.log(toDos);
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
 
-add.addEventListener("click", () => countStore.dispatch({ type: Add }));
-minus.addEventListener("click", () => countStore.dispatch({ type: Minus }));
+toDoStore.subscribe(paintToDos);
+
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  toDoStore.dispatch(deleteToDo(id));
+};
+
+const dispatchAddToDo = (text) => {
+  toDoStore.dispatch(addToDo(text));
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
